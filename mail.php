d5f636b1d584557d009ae0593508e23dcca27479
Andrew McDonald <andrew@therookies.co>
2016-05-09 15:05:13 +1000

    more stuff

--- mail.php
+++ mail.php
@@ -1,37 +1,37 @@
 <?php
 
 $to = "hello@velvetsquid.com"; # <--- Your Email 
-$subject = "VelvetSquid email form.";
+$subject = "You received a mail from your website's contact form.";
 
 if ($_POST) {
-	$name = stripslashes($_POST['fullname']);
-	$email = trim($_POST['email']);
-	$phone = stripslashes($_POST['phone']);
-	$message = htmlspecialchars($_POST['message']);
+  $name = stripslashes($_POST['fullname']);
+  $email = trim($_POST['email']);
+  $phone = stripslashes($_POST['phone']);
+  $message = htmlspecialchars($_POST['message']);
   $guests = stripslashes($_POST['guests']);
   $date = stripslashes($_POST['date']);
 
-	$error = '';
+  $error = '';
 
-	if (!$name) {
-		$error .= '<p>Enter your name</p>';
-	}
-
-	if (!$email) {
-		$error .= '<p>Enter your email</p>';
-	}
-
-	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-  	$error .= "<p>E-mail is not valid</p>";
+  if (!$name) {
+    $error .= '<p>Enter your name</p>';
   }
 
-	$mess = "Full Name: " . $name . "\r\n";
-	$mess .= "Email: " . $email . "\r\n";
-	if ($phone) {
+  if (!$email) {
+    $error .= '<p>Enter your email</p>';
+  }
+
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    $error .= "<p>E-mail is not valid</p>";
+  }
+
+  $mess = "Full Name: " . $name . "\r\n";
+  $mess .= "Email: " . $email . "\r\n";
+  if ($phone) {
     $mess .= "Phone:" . $phone . "\r\n";
   }
   if ($message) {
-  	$mess .= "Message: " . $message . "\r\n";
+    $mess .= "Message: " . $message . "\r\n";
   }
   if ($guests) {
     $mess .= "Guests:" . $guests . "\r\n";
@@ -40,23 +40,23 @@ if ($_POST) {
     $mess .= "Date:" . $date . "\r\n";
   }
 
-	if (!$error) {
-		$mail = mail($to, $subject, $mess,
-  	"From: ".$name." <".$email.">\r\n"
-  	."Reply-To: ".$name."<".$email.">\r\n"
-  	);
+  if (!$error) {
+    $mail = mail($to, $subject, $mess,
+    "From: ".$name." <".$email.">\r\n"
+    ."Reply-To: ".$name."<".$email.">\r\n"
+    );
 
-  	if ($mail) {
-  		echo "Ok";
-  	}
-  	else{
-  		header('HTTP/1.1 500 ' . $error );
+    if ($mail) {
+      header('Location: thanks.html');
+    }
+    else{
+      header('HTTP/1.1 500 ' . $error );
       exit();
-  	}
-	}
-	else{
-		echo $error;
-	}
+    }
+  }
+  else{
+    echo $error;
+  }
 }
 
 ?>
