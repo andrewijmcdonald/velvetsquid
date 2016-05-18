3c5400e716895d3d3557a83b4010b73e787bc738
Andrew McDonald <andrew@therookies.co>
2016-05-18 16:55:17 +1000

    more stuff.

--- mail.php
+++ mail.php
@@ -3,10 +3,18 @@
 $to = "hello@velvetsquid.com"; # <--- Your Email 
 $subject = "You received a mail from your website's contact form.";
 
+
 if ($_POST) {
+  # about
   $name = stripslashes($_POST['fullname']);
+  $name = stripslashes($_POST['businessname']);
   $email = trim($_POST['email']);
   $phone = stripslashes($_POST['phone']);
+  # services?
+  $service = implode(', ', $_POST['service'] );
+  # builds?
+  $build = implode(', ', $_POST['build'] );
+
   $message = htmlspecialchars($_POST['message']);
   $guests = stripslashes($_POST['guests']);
   $date = stripslashes($_POST['date']);
@@ -15,6 +23,14 @@ if ($_POST) {
 
   if (!$name) {
     $error .= '<p>Enter your name</p>';
+  }
+
+  if (!$service) {
+    $error .= '<p>Please select a service</p>';
+  }
+
+  if (!$build) {
+    $error .= '<p>Please select a type of project</p>';
   }
 
   if (!$email) {
@@ -30,6 +46,13 @@ if ($_POST) {
   if ($phone) {
     $mess .= "Phone:" . $phone . "\r\n";
   }
+  if ($build) {
+    $mess .= "Build:" . $build . "\r\n";
+  }
+  if ($service) {
+    $mess .= "Service:" . $service . "\r\n";
+  }
+
   if ($message) {
     $mess .= "Message: " . $message . "\r\n";
   }
