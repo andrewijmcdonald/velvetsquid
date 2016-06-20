d850d4dfd8a37ed3501211a31d3b9c4ab8b2bc83
Andrew McDonald <andrew@therookies.co>
2016-06-20 10:36:02 +1000

    ssl + stripe integration.

---
+++ create_subscription_pro.php
@@ -0,0 +1,27 @@
+<?php // Create a customer using a Stripe token
+
+require_once('stripe-php/init.php');
+
+// Be sure to replace this with your actual test API key
+// (switch to the live key later)
+\Stripe\Stripe::setApiKey("pk_live_6Mu19y1PM96iHc7g4dKchjWe");
+
+
+try
+{
+  $customer = \Stripe\Customer::create(array(
+    'email' => $_POST['stripeEmail'],
+    'source'  => $_POST['stripeToken'],
+    'plan' => 'wp-pro'
+  ));
+
+  header('Location: thankyou.html');
+  exit;
+}
+catch(Exception $e)
+{
+  header('Location:oops.html');
+  error_log("unable to sign up customer:" . $_POST['stripeEmail'].
+    ", error:" . $e->getMessage());
+}
+
